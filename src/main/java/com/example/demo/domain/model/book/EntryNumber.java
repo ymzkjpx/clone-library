68cf7764591422e4065c377825aca7631843409a
ymzkjpx <ymzkjpx@gmail.com>
2021-08-17 16:53:27 +0900

    toStringを修正. SQLの結果を1件に絞った

--- src/main/java/com/example/demo/domain/model/book/EntryNumber.java
+++ src/main/java/com/example/demo/domain/model/book/EntryNumber.java
@@ -1,37 +1,34 @@
 package com.example.demo.domain.model.book;
 
-import javax.validation.constraints.NotNull;
-
 /**
  * 資料番号
  */
 public class EntryNumber {
-    @NotNull(message = "資料番号を入力してください。")
     Integer value;
 
     @Deprecated
     public EntryNumber() {}
 
-    public EntryNumber(int value){this.value = (Integer)value;}
+    public EntryNumber(int value) {this.value = (Integer) value;}
+
+    public EntryNumber(String value) {
+        this.value = Integer.valueOf(value);
+    }
 
     public EntryNumber(Integer value) {
         this.value = value;
-    }
-
-    public EntryNumber(String value){
-        this.value = Integer.valueOf(value);
     }
 
     public Integer value() {
         return value;
     }
 
-    public boolean sameValue(EntryNumber other){
+    public boolean sameValue(EntryNumber other) {
         return this.value == other.value;
     }
 
     @Override
     public String toString() {
-        return "MaterialId{" + "value=" + value + '}';
+        return Integer.toString(value);
     }
 }
