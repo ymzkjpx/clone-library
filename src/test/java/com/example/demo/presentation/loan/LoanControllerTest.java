572c166b9f81501fe099542911cf9c69f31a016b
ymzkjpx <ymzkjpx@gmail.com>
2021-08-21 10:58:00 +0900

    貸出完了画面のテストを実装

--- src/test/java/com/example/demo/presentation/loan/LoanControllerTest.java
+++ src/test/java/com/example/demo/presentation/loan/LoanControllerTest.java
@@ -7,18 +7,27 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @LibraryDBTest
 @AutoConfigureMockMvc
 class LoanControllerTest {
-    @Autowired
-    MockMvc mockMvc;
-    @Test
-    void 画面
+    @Autowired MockMvc mockMvc;
 
     @Test
     void 貸出画面が表示できる() throws Exception {
-        mockMvc.perform(get("/loan/register/")).andExpect(status().isOk());
+        mockMvc.perform(get("/loan/register/"))
+               .andExpect(status().isOk());
+    }
+
+    @Test
+    void 貸出登録できる() throws Exception {
+        mockMvc.perform(post("/loan/register/").param("memberNumber.value", "1")
+                                               .param("itemNumber.value", "1-A")
+                                               .param("loanDate.value", "2021-08-20"))
+               .andExpect(status().isFound())
+               .andExpect(redirectedUrl("/loan/register/completed"));
     }
 }
